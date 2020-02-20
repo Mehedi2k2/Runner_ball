82c317076558073293becd2088371ae30e4f7657
Mehedi2k2 <60916841+Mehedi2k2@users.noreply.github.com>
2020-02-20 13:30:11 +0600

    Add files via upload

---
+++ UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+        AudioManager.instance.PlayAudio(3);
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
+        AudioManager.instance.PlayAudio(3);
+        Time.timeScale = 1;
+    }
+
+    public void home()
+    {
+        SceneManager.LoadScene(0);
+        AudioManager.instance.PlayAudio(3);
+        Time.timeScale = 1;
+    }
+
+  
+}
